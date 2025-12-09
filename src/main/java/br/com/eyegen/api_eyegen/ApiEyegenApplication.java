ad19e765443cd9bbe98ff6f4a009d249b75a19f6
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-12-08 21:18:17 -0300

    feat: adiciona estrutura e dependências iniciais do projeto

---
+++ src/main/java/br/com/eyegen/api_eyegen/ApiEyegenApplication.java
@@ -0,0 +1,13 @@
+package br.com.eyegen.api_eyegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEyegenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEyegenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eyegen/api_eyegen/ApiEyegenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eyegen.api_eyegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEyegenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
